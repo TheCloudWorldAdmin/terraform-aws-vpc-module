0f888d2c2be1a1318e0810931d301fcb26e88517
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2023-04-20 07:00:21 +0000

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA5676Z5XLRSQGX7G5"
+  secret_key = "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"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
